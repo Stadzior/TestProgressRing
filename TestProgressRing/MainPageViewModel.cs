bafb957f8ed3b5e5766d79ef7cc26915654db21c
Stadzior <Stadzior@gmail.com>
2017-09-13 22:26:01 +0200

    yup.

---
+++ TestProgressRing/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestProgressRing
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                Set(() => IsLoading, ref _isLoading, value);
+            }
+        }
+
+        public ICommand ClickCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            ClickCommand = new RelayCommand(() => 
+            IsLoading = !IsLoading);
+        }
+    }
+}
---
+++ TestProgressRing/ViewModelLocatior.cs
@@ -0,0 +1,60 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgressRing
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary> 
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                // Create design time view services and models
+            }
+            else
+            {
+                // Create run time view services and models
+            }
+
+            //Register your services used here
+            SimpleIoc.Default.Register<MainPageViewModel>();
+        }
+
+
+        // <summary>
+        // Gets the FirstPage view model.
+        // </summary>
+        // <value>
+        // The FirstPage view model.
+        // </value>
+        public MainPageViewModel MainPageViewModel
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainPageViewModel>();
+            }
+        }
+
+        // <summary>
+        // The cleanup.
+        // </summary>
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
